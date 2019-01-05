f8fb71d7c9be56e42f25cb13f928f1dfe53ca111
游里 <uuPin123@gmail.com>
2019-01-05 23:05:52 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a05d5c0869e552626fd3.js"
+    "url": "webpack-runtime-f915daa183115c10c3bf.js"
   },
   {
     "url": "app-9a714107c49954964593.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c78388247d6e54783789aeebe27661c2"
+    "revision": "aee7f8aa4bb12a73cea06cc61b5a4170"
   },
   {
     "url": "component---src-pages-404-tsx.0e4338761429b4eb16ac.css"
